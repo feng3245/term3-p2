6d9fc87ae2d0daf9273d5d0b832c88b715cabda2
Ubuntu <ubuntu@ip-172-31-1-172.us-west-1.compute.internal>
2018-02-07 02:36:45 +0000

    Increased std deviation for added uncertainty.

--- main.py
+++ main.py
@@ -58,10 +58,10 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     tf.add(output, tf.layers.conv2d(vgg_layer4_out, num_classes,1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
-     kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)))
+     kernel_initializer=tf.truncated_normal_initializer(stddev=0.05)))
     output = tf.layers.conv2d_transpose(output, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     tf.add(output,  tf.layers.conv2d(vgg_layer3_out, num_classes,1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
-     kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)))
+     kernel_initializer=tf.truncated_normal_initializer(stddev=0.05)))
     output = tf.layers.conv2d_transpose(output, num_classes, 16, 8, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     return output
 tests.test_layers(layers)
